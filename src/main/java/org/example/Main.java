08f33a0d1443e3d804041d868d02d97530cb2406
fabiobf96 <barchiesifabio96@gmail.com>
2023-10-19 16:43:50 +0200

    test 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,7 @@ public class Main {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        int a;
+        // prassi deomode
     }
 }
